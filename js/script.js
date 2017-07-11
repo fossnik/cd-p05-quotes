e2a5963e97a3f7a13e0faae23a9903d0f8403980
Zack Hartmann <zackhartmann@gmail.com>
2017-07-11 00:50:40 +0000

    Aesthetic Fixes

--- js/script.js
+++ js/script.js
@@ -4,37 +4,12 @@ $(document).ready(function(){
 function getQuote(){
 
     $.getJSON(url, function(data){
-       $(".quote").html('"'+data.quoteText+'"');
-       $(".author").html(' - '+data.quoteAuthor);
-       console.log(data);
-    });
-}
+        $(".quote").html('"'+data.quoteText+'"');
+        $(".author").html(' - '+data.quoteAuthor);
+        });
+    }
 
-    // $("#new_quote").on("click", function(){
     $("#new_quote").click(function(){
         getQuote();
     });
-    
-    $("#tweet").on("click", function(){
-        $.getJSON(url, function(data){
-        if(!data.quoteAuthor){
-            data.quoteAuthor = "unknown";
-        }
-        window.open("https://twitter.com/intent/tweet?text=" + data.quoteText + ' - Author:  '+ data.quoteAuthor, "_self");
-        });
-    });
-    
-    // youtube https://www.youtube.com/watch?v=OdtOk8keCs4  @ 6.27
-    function getInfo(){
-        FB.api('/me', 'GET', {fields: 'first_name,last_name,id'}, function(response){
-            document.getElementById('status').innerHTML = response.id;
-        });
-    }
-    
-    function post(){
-        FB.api('/me/feed', 'post', {message: 'my first status...'}, function(response){
-            document.getElementById('status').innerHTML = response.id;
-        });
-    }
-    
 });
